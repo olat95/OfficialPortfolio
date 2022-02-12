d5884f5d626b6ac01d08d9a7ce3bb103bbf12b91
Olatunji Oluwapelumi Samson <samsontunj@gmail.com>
2022-02-12 13:15:14 +0100

    changed profile pics

--- src/components/PortfolioList/PortfolioListEl.js
+++ src/components/PortfolioList/PortfolioListEl.js
@@ -1,3 +1,23 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  padding: 50px 100px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+`
+export const PortListText = styled.div`
+  width: 65%;
+`
+export const PortListTitle = styled.h1`
+  font-size: 50px;
+  font-weight: 600;
+`
+export const PortListDesc = styled.p`
+  margin: 20px 0;
+`
+export const PortList = styled.div``
+// export const Container = styled.div``
+// export const Container = styled.div``
+// export const Container = styled.div``
--- src/components/PortfolioList/index.js
+++ src/components/PortfolioList/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
-import { Container } from './PortfolioListEl'
+import Portfolio from '../Portfolio'
+import {
+  Container,
+  PortList,
+  PortListDesc,
+  PortListText,
+  PortListTitle,
+} from './PortfolioListEl'
 
 const PortfolioList = () => {
-  return <Container>PortfolioList</Container>
+  return (
+    <Container>
+      <PortListText>
+        <PortListTitle>Some of my past work</PortListTitle>
+        <PortListDesc>
+          All was done with the my knowledge of HTML5/CSS3/SCSS BOOTSTRAP4
+          JAVASCRIPT POSTGRESQL MONGODB GIT/GITHUB EXPRESSJs NODEJs REACTJs
+        </PortListDesc>
+      </PortListText>
+      <PortList>
+        <Portfolio />
+        <Portfolio />
+        <Portfolio />
+        <Portfolio />
+        <Portfolio />
+      </PortList>
+    </Container>
+  )
 }
 
 export default PortfolioList
